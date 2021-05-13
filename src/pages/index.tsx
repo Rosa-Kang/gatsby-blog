df3c1c3e6172287e979e7cfd51b4ed4a7628e15d
Rosa Kang <therosessom@gmail.com>
2021-05-12 21:26:23 -0700

    update main pages in component directory

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,23 @@
 import React, {FunctionComponent} from 'react';
-import {Link} from 'gatsby';
+import GlobalStyle from '../components/common/GlobalStyle';
+import Introduction from '../components/main/Introduction';
+import Footer from '../components/common/Footer';
+import styled from '@emotion/styled';
 
+
+const Container = styled.div `
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`
 const IndexPage: FunctionComponent = function() {
     return (
-    <div>
-        <Link to='/info/'>To Info</Link>
-    </div>    
+    <Container>
+      <GlobalStyle />
+      <Introduction />
+      <Footer/>
+    </Container>
+    
     );
 };
 
